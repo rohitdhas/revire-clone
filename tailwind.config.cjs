a081b5fc6f94e6f05343d9a3fee94b7f1d239a84
Rohit Dhas <rohitdhas666@gmail.com>
2022-07-28 14:34:51 +0530

    added new colors to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "revire-primary": "#126656",
+        "revire-orange": "#D89D55",
+        "revire-light-blue": "#CDDEE3",
+        "revire-light-orange": "#FAF5E9",
+      },
+    },
   },
   plugins: [],
 };
