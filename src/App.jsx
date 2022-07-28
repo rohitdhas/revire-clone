4a716fc5af9e8f2ebfc954ee3fa7c6a577882252
Rohit Dhas <rohitdhas666@gmail.com>
2022-07-28 14:53:42 +0530

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import Button from "./components/button";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-xl font-bold text-blue-600 text-center">
+      <h1 className="text-xl font-bold text-revire-primary text-center">
         Hello World! 🚀
       </h1>
+      <Button label="Primary" type="primary" />
+      <Button label="Secondary" type="secondary" />
     </div>
   );
 }
